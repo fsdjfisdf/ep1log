52d12afbc93df1f90acf2a6fefae6779e9b5abb2
jhw <jhw86399@naver.com>
2025-06-17 17:17:49 +0900

    saved21

--- back/config/express.js
+++ back/config/express.js
@@ -2,6 +2,8 @@ const express = require("express");
 const compression = require("compression");
 const methodOverride = require("method-override");
 var cors = require("cors");
+const path = require("path");
+
 
 module.exports = function () {
   const app = express();
@@ -12,8 +14,12 @@ module.exports = function () {
   app.use(express.urlencoded({ extended: true })); // form 으로 제출되는 값 파싱
   app.use(methodOverride()); // put, delete 요청 처리
   app.use(cors()); // 웹브라우저 cors 설정을 관리
-  app.use(express.static("/home/ubuntu/food-map-dist-example/front")); // express 정적 파일 제공 (html, css, js 등..)
+  app.use(express.static(path.join(__dirname, "../../front")));
   // app.use(express.static(process.cwd() + '/public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../front/index.html"));
+});
 
   /* 직접 구현해야 하는 모듈 */
   require("../src/routes/indexRoute")(app);
