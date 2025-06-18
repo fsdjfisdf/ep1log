c1b1da5242912e0061e0e35255f8d89106e8543a
jhw <jhw86399@naver.com>
2025-06-18 10:07:20 +0900

    saved21

--- back/index.js
+++ back/index.js
@@ -1,11 +1,9 @@
 const express = require("./config/express");
 const app = express();
-const { logger } = require("./config/winston"); // log
+const { logger } = require("./config/winston");
 
 const port = 3001;
-express().listen(port);
-logger.info(`API Server Start At Port ${port}`);
 
 app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  logger.info(`🚀 API Server started on http://localhost:${port}`);
 });
