0d4ee6b7d6f48845b17d6fb09e68f2b893eb9be1
jhw <jhw86399@naver.com>
2025-06-18 10:01:15 +0900

    saved21

--- back/src/routes/reservationRoute.js
+++ back/src/routes/reservationRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/reservationController');
+console.log("✅ reservationRoute loaded");
 
 router.post('/reservation', controller.createReservation);
 
