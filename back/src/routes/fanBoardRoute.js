3ee9c213f3856c10fbc825003f193ff0bf4fcfde
jhw <jhw86399@naver.com>
2025-06-24 16:23:02 +0900

    saved30

--- back/src/routes/fanBoardRoute.js
+++ back/src/routes/fanBoardRoute.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/fanBoardController');
 
-console.log("✅ fanBoardRoute loaded");
 
-router.post('/fanboard', controller.createPost);
+console.log('✅ fanBoardRoute loaded');
+
+router.post('/fanboard', controller.createPost); // ✅ createPost 정의돼 있어야 함
 router.get('/fanboard', controller.getAllPosts);
 router.delete('/fanboard/:id', controller.deletePost);
 
